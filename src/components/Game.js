bddb12d7af13b23243e2b8b1d4b11fa17adf5634
dashahello <st.daryaa@gmail.com>
2021-04-14 19:51:28 +0200

    + 'x' and 'o' when board  is clicked

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,7 +7,15 @@ const Game = () => {
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(board);
 
-  const handleClick = () => {};
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    // If user click an occupied square or if game is won, return
+    if (winner || boardCopy[i]) return;
+    // Put an X or an O in the clicked square
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  };
 
   const jumpTo = () => {};
 
