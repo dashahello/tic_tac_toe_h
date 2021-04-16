61eaac2812febf42dc7cbb6bf27d1391fa60f139
dashahello <st.daryaa@gmail.com>
2021-04-16 23:01:46 +0200

    + timetravel

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { calculateWinner } from '../helpers.js';
-import Board from './Board.js';
+import { calculateWinner } from '../helpers';
+import Board from './Board';
 
 const styles = {
   width: '200px',
@@ -8,29 +8,42 @@ const styles = {
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
-    const boardCopy = [...board];
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
     // If user click an occupied square or if game is won, return
-    if (winner || boardCopy[i]) return;
+    if (winner || squares[i]) return;
     // Put an X or an O in the clicked square
-    boardCopy[i] = xIsNext ? 'X' : 'O';
-    setBoard(boardCopy);
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXisNext(!xIsNext);
   };
 
-  const jumpTo = () => {};
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
 
-  const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>START GAME</button>
-  );
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move#${move}` : 'Go to start';
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
 
   return (
     <>
-      <Board squares={board} onClick={handleClick} />;
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <div style={styles}>
         <p>
           {winner
